5ee3561b5c3726cad282d35b196622725e58ec08
damian_pileckas <damianpileckas@itrsa.com.ar>
2021-05-04 00:50:26 -0300

    backend heroku

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,16 +6,20 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SpotifyService {
+  private token: any;
+
 
   constructor(private http: HttpClient) { 
-    
+    this.getToken()
+      .subscribe(resp => {
+        this.token = resp;
+      });  
   }
   
   getQuery( query: string){
     const url = `https://api.spotify.com/v1/${ query }`;
-
     const headers = new HttpHeaders({
-      'Authorization':'Bearer BQBwoot7wERQs6m-GmB3_Bcb2RvMtFZsXLBRZHCTv1rXB6msAJr-j0hDmme5oCQCPytOPoG38W_xMrciiVE'
+      'Authorization':`Bearer ${this.token.access_token}`
     });
 
     return this.http.get(url, {headers});
@@ -43,4 +47,10 @@ export class SpotifyService {
     return this.getQuery(`artists/${id}/top-tracks?market=us`)
       .pipe( map( data => data['tracks']));
   }
+
+  getToken(): any {
+    return this.http.get(`https://spotify-ar-api.herokuapp.com/spotify/49c58326fd8e4b62a56efd9732327ada/1ac83de3b84f4d828845bdda1743d561`);
+  }
 }
+
+
